1852beffe211e06027a2c7e17b3f586dd68e2062
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-07-15 23:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const person = {
   age: 80,
 };
 
-let team = players;
+let team = players;// players[0] ->team[0]
 let team1 = [].concat(players);
-let cap1 = Object.assign({},person);
+let cap1 = {...person}
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
